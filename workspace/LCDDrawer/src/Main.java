2df4cf19c34ce7dbb30d390beb8e937a6658bd95
Piotr Lichwala <piotr.lichwala@tomtom.com>
2014-07-15 22:16:28 +0200

    LCD test - first application

---
+++ workspace/LCDDrawer/src/Main.java
@@ -0,0 +1,12 @@
+import lejos.hardware.lcd.LCD;
+import lejos.utility.Delay;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		LCD.drawString("Plugin Test", 0, 4);
+		Delay.msDelay(5000);
+	}
+
+}
